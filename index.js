cd2429346eb3718ffff628683ca7b7d98ad99389
Pradyu <pvasu009@uottawa.ca>
2023-06-06 17:37:56 -0400

    Finished dropdown for service and barber

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 $("#bookNowMainButton").on("click", function(){
     window.location.href = 'booknow.html';
 });
+
+
+$(document).ready(function() {
+    $('.dropdown-menu a.dropdown-item').click(function() {
+      var selectedOption = $(this).text();
+      $(this).closest('.dropdown').find('.dropdown-toggle').html(selectedOption + ' <span class="caret"></span>');
+    });
+  });
