486a61eb0e72f01b8d1ce58a7a155f93ec895a2f
Pradyu <pvasu009@uottawa.ca>
2023-06-06 17:57:31 -0400

    added date selection and some validation to it

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 $("#bookNowMainButton").on("click", function(){
-    window.location.href = 'booknow.html';
+    window.location.href = 'booknow.html'; //redirects to booknow.html
 });
 
+//Sets minimum date possible in book now to be current date of usage
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0
+var yyyy = today.getFullYear();
 
-$(document).ready(function() {
-    $('.dropdown-menu a.dropdown-item').click(function() {
-      var selectedOption = $(this).text();
-      $(this).closest('.dropdown').find('.dropdown-toggle').html(selectedOption + ' <span class="caret"></span>');
-    });
-  });
+today = yyyy + '-' + mm + '-' + dd;
+document.getElementById("dateToBook").setAttribute("min", today);
