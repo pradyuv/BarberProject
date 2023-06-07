9b89f83932c8fcff5ddcd3884847411c1477e0f5
Pradyu <pvasu009@uottawa.ca>
2023-06-07 12:50:09 -0400

    fixed consult and cut in index.html, organized js file a bit

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-$("#bookNowMainButton").on("click", function(){
-    window.location.href = 'booknow.html'; //redirects to booknow.html
-});
+const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
-const re =
-  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+if (window.location.href.endsWith("booknow.html")){ //only executes in booknow, if I didn't have this conditional, this logic would fail in other webpages
+    
+    
+    //Sets minimum date possible in book now to be current date of usage
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0
+    var yyyy = today.getFullYear();
+    
+    today = yyyy + '-' + mm + '-' + dd;
+    document.getElementById("dateToBook").setAttribute("min", today);
+    
+}
 
-//Sets minimum date possible in book now to be current date of usage
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0
-var yyyy = today.getFullYear();
+if (window.location.href.endsWith("index.html")){ //only occurs in index.html, so that the logic isn't blocked elsewhere
+    $("#bookNowMainButton").on("click", function(){
+        window.location.href = 'booknow.html'; //redirects to booknow.html
+    });
+    
+}
 
-today = yyyy + '-' + mm + '-' + dd;
-document.getElementById("dateToBook").setAttribute("min", today);
-
+//consult and cut validation -> works on booknow and index
 document.getElementById("submitConsult").addEventListener("click", function(event) {
     var emailInput = document.getElementById("emailConsultAndCut");
     var email = emailInput.value.trim();
