579c1ba986ee9e4415e1a4d80cf0a2744835d5d6
Pradyu <pvasu009@uottawa.ca>
2023-06-07 15:36:31 -0400

    fixed form validation, confirmation is only a modal now

--- index.js
+++ index.js
@@ -1,81 +1,62 @@
 const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
-if (window.location.href.endsWith("booknow.html")){ //only executes in booknow, if I didn't have this conditional, this logic would fail in other webpages
-    
-    
-    //Sets minimum date possible in book now to be current date of usage
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0
-    var yyyy = today.getFullYear();
-    
-    today = yyyy + '-' + mm + '-' + dd;
-    document.getElementById("dateToBook").setAttribute("min", today);
-    
+if (window.location.href.endsWith("booknow.html")) {
+  // Only executes in booknow.html
+
+  // Sets minimum date possible in book now to be the current date of usage
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0
+  var yyyy = today.getFullYear();
+
+  today = yyyy + '-' + mm + '-' + dd;
+  document.getElementById("dateToBook").setAttribute("min", today);
 }
 
-if (window.location.href.endsWith("index.html")){ //only occurs in index.html, so that the logic isn't blocked elsewhere
-    $("#bookNowMainButton").on("click", function(){
-        window.location.href = 'booknow.html'; //redirects to booknow.html
-    });
-    
+if (window.location.href.endsWith("index.html")) {
+  // Only occurs in index.html, so that the logic isn't blocked elsewhere
+  $("#bookNowMainButton").on("click", function() {
+    window.location.href = 'booknow.html'; // Redirects to booknow.html
+  });
 }
 
-//consult and cut validation
+// Consult and cut validation
 document.getElementById("submitConsult").addEventListener("click", function(event) {
-    var validationPass = validateEmail("emailConsultAndCut");
-    if (validationPass){
-        $('#staticBackdrop').modal('show');
-    }    
-  });
-
-  document.getElementById("submitBooking").addEventListener("click", function(event) {
-    validateEmail("emailAddressInputBooking");
-    var formPass = validateBookNow("bookNowForm");
-    if (formPass){
-        $("#submitBooking").on("click", function(){
-            window.location.href = 'confirmationpage.html'; //redirects to confirmationpage.html
-        });
-    }
-  });
-
-
-  
-
-  function validateEmail(inputId){
-    var emailInput = document.getElementById(inputId);
-    var email = emailInput.value.trim();
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    
-    if (!emailRegex.test(email)) {
-      // Prevent the form submission
-      event.preventDefault();
-      emailInput.setCustomValidity("Please enter a valid email address");
-      emailInput.reportValidity();
-      return false;
-    } else {
-      // Valid email, allow form submission and show the modal
-      emailInput.setCustomValidity("");
-      emailInput.reportValidity();
-      return true;
-    }
-  };
-
-  
-
-  // Clear the validation message when the email input value changes
-  document.getElementById("emailConsultAndCut").addEventListener("input", function() {
-    this.setCustomValidity("");
-  });
-
-  function doneConsult() {
-    location.reload(); //reloads webpage when consult is done
+  var validationPass = validateEmail("emailConsultAndCut");
+  if (validationPass) {
+    $('#staticBackdrop').modal('show');
   }
+});
 
-  function validateBookNow(formId){
-    var form = document.getElementById(formId);
-    var elements = form.elements;
-    var isValid = true;
+document.getElementById("submitBooking").addEventListener("click", function(event) {
+  event.preventDefault(); // Prevents the form submission
+
+  var validationPass = validateEmail("emailAddressInputBooking");
+  if (validationPass) {
+    validateBookNow("bookNowForm");
+  }
+});
+
+function validateEmail(inputId) {
+  var emailInput = document.getElementById(inputId);
+  var email = emailInput.value.trim();
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!emailRegex.test(email)) {
+    emailInput.setCustomValidity("Please enter a valid email address");
+    emailInput.reportValidity();
+    return false;
+  } else {
+    emailInput.setCustomValidity("");
+    emailInput.reportValidity();
+    return true;
+  }
+}
+
+function validateBookNow(formId) {
+  var form = document.getElementById(formId);
+  var elements = form.elements;
+  var isValid = true;
 
   for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
@@ -96,10 +77,18 @@ document.getElementById("submitConsult").addEventListener("click", function(even
   }
 
   if (isValid) {
+    $('#confirmModal').modal('show'); // Show the modal if the form is valid
     console.log("Form is valid. Submitting...");
-    return true;
   } else {
     console.log("Form is not valid. Please fill out all required fields.");
-    return false;
   }
-  }
+}
+
+// Clear the validation message when the email input value changes
+document.getElementById("emailConsultAndCut").addEventListener("input", function() {
+  this.setCustomValidity("");
+});
+
+function doneConsult() {
+  location.reload(); // Reloads webpage when consult is done
+}
