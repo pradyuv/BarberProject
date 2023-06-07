a26f9bc867749a984539eb0f9501e85b3dbf613a
Pradyu <pvasu009@uottawa.ca>
2023-06-07 10:50:10 -0400

    Modal validation not working yet

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ $("#bookNowMainButton").on("click", function(){
     window.location.href = 'booknow.html'; //redirects to booknow.html
 });
 
+const re =
+  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
 //Sets minimum date possible in book now to be current date of usage
 var today = new Date();
 var dd = String(today.getDate()).padStart(2, '0');
@@ -10,3 +13,13 @@ var yyyy = today.getFullYear();
 
 today = yyyy + '-' + mm + '-' + dd;
 document.getElementById("dateToBook").setAttribute("min", today);
+
+function validateInput() {
+    var input = document.getElementById('emailConsultAndCut').value;
+    // Add your input validation logic here
+    if (input === re) {
+      $('#staticBackdrop').modal('show');
+    }else{
+        input.setCustomValidity("Invalid email.");
+    }
+  }
